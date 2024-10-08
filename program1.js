6985dcc6ec74f8477015839e566fb6380aac79d2
amitpawar <amitpawar7475@gmail.com>
2024-10-08 12:18:52 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -6,6 +6,8 @@ const getTotalIsles = function (grid) {
   const n = grid.length;
   const m = grid[0].length;
 
+  
+
   function dfs(i, j) {
     if (grid[i][j] === '1') grid[i][j] = '0';
     else return;
