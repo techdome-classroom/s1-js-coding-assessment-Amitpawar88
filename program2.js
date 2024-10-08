629add03936c295a0c41234da5141ab7238e637f
amitpawar <amitpawar7475@gmail.com>
2024-10-08 12:32:43 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,7 @@ const decodeTheRing = function (s, p) {
     // write your code here
     const n = s.length;
     const m = p.length;
-    const dp = 
+    const dp = new Array(n + 1).fill().map(() => new Array(n + 1))
 
   };
   
