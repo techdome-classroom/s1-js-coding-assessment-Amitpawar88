eb5091163a69bf607946c98b8b3bf8fcb878bcf1
amitpawar <amitpawar7475@gmail.com>
2024-10-08 12:40:49 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,29 +2,29 @@ const decodeTheRing = function (s, p) {
 
     // write your code here
     const n = s.length;
-  const m = p.length;
+    const m = p.length;
 
-  const dp = Array(n + 1).fill(false).map(() => Array(m + 1).fill(false));
+    const dp = Array(n + 1).fill(false).map(() => Array(m + 1).fill(false));
 
-  dp[0][0] = true;
+    dp[0][0] = true;
 
-  for (let j = 1; j <= m; j++) { 
-    if (p[j - 1] === '*') {  
-      dp[0][j] = dp[0][j - 1];  
-    }
-  }
-
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= m; j++) {
-      if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {  
-        dp[i][j] = dp[i - 1][j - 1];  
-      } else if (p[j - 1] === '*') {  
-        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];  
+    for (let j = 1; j <= m; j++) { 
+      if (p[j - 1] === '*') {  
+        dp[0][j] = dp[0][j - 1];  
       }
     }
-  }
 
-  return dp[n][m];
+    for (let i = 1; i <= n; i++) {
+      for (let j = 1; j <= m; j++) {
+        if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {  
+          dp[i][j] = dp[i - 1][j - 1];  
+        } else if (p[j - 1] === '*') {  
+          dp[i][j] = dp[i][j - 1] || dp[i - 1][j];  
+        }
+      }
+    }
+
+    return dp[n][m];
 };
   
   module.exports = decodeTheRing;
